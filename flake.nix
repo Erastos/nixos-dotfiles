794567523b251852c5478d56c5736b330049fc99
Andrew Smith <andrew@andrewnsmith.net>
2025-07-05 17:41:51 -0400

    Remove hardware and update flake

--- flake.nix
+++ flake.nix
@@ -2,18 +2,15 @@
   description = "Erastos Config Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware.url = "github:/NixOS/nixos-hardware/master";
-    hyprland.url = "github:hyprwm/Hyprland";
-    poetry2nix.url = "github:nix-community/poetry2nix/";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     
 };
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, hyprland, poetry2nix, ...}@inputs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ...}@inputs: 
     let 
       unstable = import nixpkgs-unstable {
         system = "x86_64-linux";
@@ -21,31 +18,7 @@
       };
     in
   {
-    nixosConfigurations.Rogue = nixpkgs.lib.nixosSystem {
-      specialArgs = inputs;
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
-      modules = 
-      [
-       ./hosts/system/Rogue.nix 
-       ./hardware/Rogue.nix
-       ./modules/podman.nix
-       ./modules/neo4j.nix
-       nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
-       home-manager.nixosModules.home-manager {
-           home-manager.useGlobalPkgs = true; 
-           home-manager.useUserPackages = true;
-           home-manager.users.erastos = {
-             imports = [ ./hosts/home/Rogue.nix ];
-           };
-         }
-      ];
-
-    };
-
-    nixosConfigurations.Serenity = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.Trinity = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs unstable;};
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -54,29 +27,18 @@
 
       modules = 
       [
-       ./hosts/system/Serenity.nix 
-       ./hardware/Serenity.nix
-       # ./modules/i3
-       ./modules/plasma.nix
-       # ./modules/lightdm.nix
-       ./modules/gtk.nix
-       ./modules/steam.nix
-       ./modules/security.nix
-
-       ./modules/neo4j.nix
-       # ./modules/rofi-themes.nix
+       /etc/nixos/configuration.nix
        home-manager.nixosModules.home-manager {
 	   home-manager.extraSpecialArgs = {inherit unstable;};
            home-manager.useGlobalPkgs = true; 
            home-manager.useUserPackages = true;
            home-manager.users.erastos = {
-             imports = [ ./hosts/home/Serenity.nix ];
+             imports = [ ];
            };
          }
 
       ];
 
     };
-};
-
+  };
 }
