c848bd53ac7e630ce3cf3a91a62c2279edbc77ef
Andrew Smith <andrew@andrewnsmith.net>
2025-07-14 15:36:31 -0400

    update tmux config

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -5,18 +5,33 @@
 programs.tmux = {
   enable = true;
   prefix = "C-a";
+  historyLimit = 10000;
   extraConfig = ''
+  # remap prefix from 'C-b' to 'C-a'
+  # unbind C-b
+  # set-option -g prefix C-a
+  # bind-key C-a send-prefix
+
   # split panes using | and -
   bind | split-window -h
   bind - split-window -v
   unbind '"'
-  unbind %  
+  unbind %
 
-  # switch panes using Alt-arrow without prefix
   bind h select-pane -L
   bind l select-pane -R
   bind k select-pane -U
   bind j select-pane -D
+
+  bind -T copy-mode u send -X page-up
+  bind -T copy-mode f send -X page-down
+
+  bind C-u copy-mode -u
+
+  set -g @plugin 'tmux-plugins/tpm'
+  set -g @plugin 'tmux-plugins/tmux-resurrect'
+
+  run '/usr/share/tmux-plugin-manager/tpm'
   '';
 };
 
