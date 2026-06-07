75e7e4a79fc31e49494ceaf82c5ee21f31913522
Andrew Smith <andrew@andrewnsmith.net>
2026-06-07 11:33:08 -0400

    flake update

--- modules/home/coding-agents.nix
+++ modules/home/coding-agents.nix
@@ -8,7 +8,7 @@
     (_: prev: {
       pi-coding-agent = prev.pi-coding-agent.overrideAttrs (old: {
         npmDeps = old.npmDeps.overrideAttrs (_: {
-          outputHash = "sha256-vNXInQTvB8h8/8PA0WcQ69Ni+pABK7n9wMr3pY3aY4c=";
+          outputHash = "sha256-7zLvhuhyOD/p5xCZ4hsCdFH9E9AQFu8HCOsjSxMB74o=";
         });
       });
     })
