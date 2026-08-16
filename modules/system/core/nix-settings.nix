57a94952f2c75b5b32bd47ee2f17a844ba37bac0
Andrew Smith <andrew@andrewnsmith.net>
2026-08-16 18:58:21 -0400

    update flake and add nh

--- modules/system/core/nix-settings.nix
+++ modules/system/core/nix-settings.nix
@@ -6,4 +6,10 @@
     auto-optimise-store   = true;
   };
   nix.package = pkgs.unstable.lix;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/netscape/nixos-dotfiles/";
+  };
 }
