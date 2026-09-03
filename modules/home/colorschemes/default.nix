19094346a8983da2deb1020a5fe64fed29a19e8f
Andrew Smith <andrew@andrewnsmith.net>
2026-09-02 20:22:12 -0400

    Add 2000 Enterprise Theme

--- modules/home/colorschemes/default.nix
+++ modules/home/colorschemes/default.nix
@@ -4,4 +4,5 @@
   burgundy-red = import ./burgundy-red.nix { inherit lib; };
   blue-matrix = import ./blue-matrix.nix { inherit lib; };
   cyberpunk-neon = import ./cyberpunk-neon.nix { inherit lib; };
+  enterprise-2000 = import ./enterprise-2000.nix { inherit lib; };
 }
